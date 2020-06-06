06ec558eeb3e9a321d4f482033cb47dc68dba20f
Dan Suter <dan@antidote.org.nz>
2020-06-07 08:04:59 +1200

    remove comments sql

--- files/facebook/Krizen/import.sql
+++ files/facebook/Krizen/import.sql
@@ -1,7 +1,3 @@
-CREATE TABLE IF NOT EXISTS `Users` (
-  `UID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `Fuid` varchar(100) NOT NULL,
-  `Ffname` varchar(60) NOT NULL,
-  `Femail` varchar(60) DEFAULT NULL,
-  PRIMARY KEY (`UID`)
-);
+version https://git-lfs.github.com/spec/v1
+oid sha256:3feb1259ca1a2b94491779b38c121340b277934b5333e0ad63ac854cefb48bc1
+size 222
