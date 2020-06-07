8decfc4ad200626afb669449b2cb0ba756291c18
Dan Suter <54545927+dansuter-nz@users.noreply.github.com>
2020-06-08 11:02:25 +1200

    Update settings.php

--- admin/settings.php
+++ admin/settings.php
@@ -1,17 +1,15 @@
 <?php
 //settings database connection
 $dbservername = "localhost";
-$bdusername = "antidote_web";
-$dbpassword = "Freeyourmind32!";
+$bdusername = "your_db_user";
+$dbpassword = "your_db_password";
 $dbname = "antidote";
 
 //email for swift_SMTP
-$smtpserver='smtp.zoho.com';
-$smtpport=587;
+$smtpserver='smtp.zoho.com'; // add your smtp here
+$smtpport=0; //eg 587
 $smtpsecurity='tls';
 
-$smtpuser='dan@antidote.org.nz';
-$smtppass='1hdQrdLkgkap';
-
-
+$smtpuser='your_user_name';
+$smtppass='your_password';
 ?>
