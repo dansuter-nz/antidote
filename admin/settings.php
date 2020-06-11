e9796450512f7dce3c61f0a5933bc510b31ad76c
Dan Suter <54545927+dansuter-nz@users.noreply.github.com>
2020-06-11 15:19:11 +1200

    Update settings.php

--- admin/settings.php
+++ admin/settings.php
@@ -1,7 +1,7 @@
 <?php
 $_SESSION["dbservername"] = "localhost";
-$_SESSION["dbusername"] = "antidote_web";
-$_SESSION["dbpassword"] = "Freeyourmind32!";
+$_SESSION["dbusername"] = "Your_db_username_goes_here";
+$_SESSION["dbpassword"] = "Your_password_goes_here!";
 $_SESSION["dbname"] = "antidote";
 
 ?>
