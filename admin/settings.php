3231db5f53c4bebf23e4b0ecb3a91b2c967cdea2
dansuter-nz <dansuternz@gmail.com>
2020-06-11 14:40:23 +1200

    udpate security

--- admin/settings.php
+++ admin/settings.php
@@ -1,8 +1,15 @@
 <?php
 //settings database connection
 $dbservername = "localhost";
-$bdusername = "your_db_user";
-$dbpassword = "your_db_password";
+$bdusername = "antidote_web";
+$dbpassword = "Make_a_password!:";
 $dbname = "antidote";
 
+//email for swift_SMTP
+$smtpserver='smtp.zoho.com';//chnge this to your smtp
+$smtpport=587; //sample only
+$smtpsecurity='tls'; //sample only
 
+$smtpuser='dan@antidote.org.nz';//email me if you get stuck
+$smtppass='api_password_from_smtp';
+?>
